0d6b79b2198cf990a531958f4b1cc45adf9a9170
FloBay <f.bayer@tum.de>
2025-02-11 00:26:23 +0100

    Partial #16 - Add more tests for thresholding

--- tests/unit/test_thresholding.py
+++ tests/unit/test_thresholding.py
@@ -3,6 +3,26 @@ import pandas as pd
 import pytest
 import curve_curator.thresholding as thresholding
 
+#
+# Helper functions
+#
+def get_empty_df(n_rows, decoy=False):
+    # Create defaults
+    df = pd.DataFrame({
+        'Name' : range(n_rows),
+        'Score' : np.full(n_rows, fill_value=0.0),
+        'Decoy' : np.full(n_rows, fill_value=decoy),
+    })
+
+    # Make nice names
+    name_label = 'Decoy' if decoy else 'Target'
+    df['Name'] = name_label + ' ' + df['Name'].astype(str)
+    return df
+
+
+#
+# Actual test classes
+#
 
 class TestS0:
     fc_lim = 1.0
@@ -257,6 +277,116 @@ class TestSAMcorrection:
         expected_f_adjs = np.array([1/0.1,  1/0.15, 1/0.2, 1/0.3, 1/0.6, 0])**2
         f_adjs = thresholding.sam_correction(f_values, curve_fold_change, s0)
         assert all(np.isclose(expected_f_adjs, f_adjs))
+
+
+class TestCalculateQValues:
+
+    decoy_col = 'Decoy'
+    sort_cols = ['Score']
+    sort_ascendings = [False]
+    q_col_name = 'q-value'
+
+    def test_qvalues_all_one(self):
+        # Create data
+        target_df = get_empty_df(100, decoy=False)
+        target_df['Score'] = np.linspace(0, 10, 100)
+        decoy_df = get_empty_df(100, decoy=True)
+        decoy_df['Score'] = np.linspace(0, 10, 100)
+        df_in = pd.concat([target_df, decoy_df], ignore_index=True)
+        # Apply function
+        df_out = thresholding.calculate_qvalue(df_in, self.sort_cols, self.sort_ascendings, self.decoy_col, self.q_col_name)
+        # Tests:
+        assert self.q_col_name in df_out.columns
+        expected_q = np.full(200, 1.0)
+        assert all(np.isclose(expected_q, df_out[self.q_col_name].values))
+
+    def test_qvalues_monotonic(self):
+        # Create data
+        target_df = get_empty_df(99, decoy=False)
+        target_df['Score'] = np.array([np.linspace(1, 9, 9) for i in range(11)]).flatten()
+        decoy_df = get_empty_df(99, decoy=True)
+        decoy_df['Score'] = np.linspace(0.01, 0.99, 99)
+        df_in = pd.concat([target_df, decoy_df], ignore_index=True)
+        # Apply function
+        df_out = thresholding.calculate_qvalue(df_in, self.sort_cols, self.sort_ascendings, self.decoy_col, self.q_col_name)
+        # Tests
+        expected_q = np.append(np.full(99, 0.01), np.linspace(1.0, 0.02, 99))
+        assert all(np.isclose(expected_q, df_out[self.q_col_name].values))
+
+
+class TestGetFDR:
+
+    col = 'Score'
+    threshold = 2
+    target_decoy_ratio = 1.0
+
+    def test_all_zero_score(self):
+        target_df = get_empty_df(100, decoy=False)
+        decoy_df = get_empty_df(100, decoy=True)
+
+        expected_fdr = 1.0
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_all_pass_score(self):
+        target_df = get_empty_df(100, decoy=False)
+        target_df[self.col] = np.full(100, 3.0)
+        decoy_df = get_empty_df(100, decoy=True)
+        decoy_df[self.col] = np.full(100, 3.0)
+
+        expected_fdr = 1.0
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_halve_pass_score(self):
+        target_df = get_empty_df(100, decoy=False)
+        target_df[self.col] = np.append(np.full(50, 3.0), np.full(50, 1.0))
+        decoy_df = get_empty_df(100, decoy=True)
+        decoy_df[self.col] = np.append(np.full(50, 3.0), np.full(50, 1.0))
+
+        expected_fdr = 1.0
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_target_only(self):
+        target_df = get_empty_df(100, decoy=False)
+        target_df[self.col] = np.full(100, 3.0)
+        decoy_df = get_empty_df(100, decoy=True)
+
+        expected_fdr = 1/101
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_decoy_only(self):
+        target_df = get_empty_df(100, decoy=False)
+        decoy_df = get_empty_df(100, decoy=True)
+        decoy_df[self.col] = np.full(100, 3.0)
+
+        expected_fdr = 1.0
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_10pct_fdr(self):
+        target_df = get_empty_df(99, decoy=False)
+        target_df[self.col] = np.full(99, 3.0)
+        decoy_df = get_empty_df(99, decoy=True)
+        decoy_df[self.col] = np.append(np.full(9, 3.0), np.full(90, 1.0))
+
+        expected_fdr = 0.1
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, self.target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
+    def test_taregt_decoy_ratio(self):
+        target_df = get_empty_df(99, decoy=False)
+        target_df[self.col] = np.full(99, 3.0)
+        decoy_df = get_empty_df(299, decoy=True)
+        decoy_df[self.col] = np.append(np.full(29, 3.0), np.full(270, 1.0))
+
+        target_decoy_ratio = 1/3
+        expected_fdr = 0.1
+        fdr = thresholding.get_fdr(target_df, decoy_df, self.col, self.threshold, target_decoy_ratio)
+        assert np.isclose(expected_fdr, fdr)
+
 
 
 class TestDefineRegulatedCurves:
